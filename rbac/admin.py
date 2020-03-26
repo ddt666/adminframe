656f2328ddedad06b38a56749f53a9863d6384b0
ml <ml@126.com>
2020-03-26 16:01:09 +0800

    rbac admin添加基本数据

--- rbac/admin.py
+++ rbac/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from rbac import models
+
 
 # Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ["title", "icon", "weight"]
+    list_editable = ["icon", "weight"]
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ["title", "url", "menu", "is_menu", "parent", "alias"]
+    list_editable = ["url", "menu", "is_menu", "parent", "alias"]
+
+
+admin.site.register(models.Role)
+admin.site.register(models.Permission,PermissionAdmin)
+admin.site.register(models.Menu, MenuAdmin)
